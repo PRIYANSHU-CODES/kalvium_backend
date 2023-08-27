f3e18310eced2cc5306ba99883d271f12fc016cb
PRIYNSHU-CODES <pk1717@srmist.edu.in>
2023-08-27 17:00:04 +0530

    fixed error

--- express/index.js
+++ express/index.js
@@ -3,67 +3,57 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
-const PORT = 3000;
-const dbUrl = process.env.dburl;
-
+const SERVER_PORT = 3000;
+const DB_URL = process.env.dburl;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const OperationSchema = new mongoose.Schema({
-    question: String,
-    answer: Number
+const CalculationSchema = new mongoose.Schema({
+    expression: String,
+    result: Number
 });
 
-const Operation = mongoose.model('Operation', OperationSchema);
+const Calculation = mongoose.model('Calculation', CalculationSchema);
 
 app.get('/', (req, res) => {
     res.send(`
-        <h2>Sample Endpoints:</h2>
-        <ul>
-            <li>/5/plus/3</li>
-            <li>/3/minus/5</li>
-            <li>/3/minus/5/plus/8</li>
-            
-        </ul>
+        <h2>Sample Input: /5/plus/3</h2>
     `);
 });
 
 app.get('/history', async (req, res) => {
     try {
-        let operations = await Operation.find().limit(20).sort({ _id: -1 });
-        let historyHtml = operations.map(op => `<li>${op.question} = ${op.answer}</li>`).join('');
+        let calculations = await Calculation.find().limit(20).sort({ _id: -1 });
+        let historyHtml = calculations.map(calc => `<li>${calc.expression} = ${calc.result}</li>`).join('');
         res.send(`
-            <h2>Last 20 operations:</h2>
+            <h2>Last 20 calculations:</h2>
             <ul>
                 ${historyHtml}
             </ul>
         `);
     } catch (error) {
-        res.status(500).send('Error retrieving operations.');
+        res.status(500).send('Error retrieving calculations.');
     }
 });
-
-
 
 app.get('/*', async (req, res) => {
     const mathParts = req.path.slice(1).split('/');
 
     if (mathParts.length % 2 === 0) {
-        return res.status(400).send({ error: 'Invalid operation format' });
+        return res.status(400).send({ error: 'Invalid calculation format' });
     }
 
-    let question = mathParts[0];
+    let expression = mathParts[0];
     let values = [parseInt(mathParts[0], 10)];
 
     for (let i = 1; i < mathParts.length; i += 2) {
         const operator = mathParts[i];
         const operand = parseInt(mathParts[i + 1], 10);
 
-        question += operatorSymbol(operator) + mathParts[i + 1];
+        expression += operatorSymbol(operator) + mathParts[i + 1];
         values.push(operator);
         values.push(operand);
     }
@@ -76,8 +66,8 @@ app.get('/*', async (req, res) => {
                 if (values[i + 1] === 0) return res.status(400).send({ error: 'Division by zero is not allowed' });
                 values[i - 1] /= values[i + 1];
             }
-            values.splice(i, 2);  
-            i--;  
+            values.splice(i, 2);
+            i--;
         }
     }
 
@@ -91,20 +81,20 @@ app.get('/*', async (req, res) => {
     }
 
     try {
-        const operation = new Operation({ question, answer: result });
-        await operation.save();
+        const calculation = new Calculation({ expression, result });
+        await calculation.save();
 
-        const operationsCount = await Operation.countDocuments();
-        if (operationsCount > 20) {
-            const operations = await Operation.find().sort({ _id: 1 }).limit(operationsCount - 20);
-            for (let op of operations) {
-                await op.remove();
+        const calculationsCount = await Calculation.countDocuments();
+        if (calculationsCount > 20) {
+            const calculations = await Calculation.find().sort({ _id: 1 }).limit(calculationsCount - 20);
+            for (let calc of calculations) {
+                await calc.remove();
             }
         }
 
-        res.json({ question, answer: result });
+        res.json({ expression, result });
     } catch (error) {
-        res.status(500).send('Error saving operation.');
+        res.status(500).send('Error saving calculation.');
     }
 });
 
@@ -118,6 +108,6 @@ function operatorSymbol(operator) {
     }
 }
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running at http://localhost:${SERVER_PORT}`);
 });
